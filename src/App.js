02dd3040af9a3e6d10b4403e61a6c2b65964e9fc
julenia <julia.jankowska@onet.eu>
2019-07-15 12:21:32 +0200

    progress

--- src/App.js
+++ src/App.js
@@ -1,26 +1,45 @@
-import React from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Title from './components/Title'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Welcome to Codaisseur
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to Codaisseur</h1>
+        </header>
+        <main>
+          <Title content ="Some simple content"/>
+        </main>
+      </div>
+    );
+  }
 }
+// function App() {
+//   return (
+//     <div className=“App”>
+//       <header className=“App-header”>
+//         <img src={logo} className=“App-logo” alt=“logo” />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className=“App-link”
+//           href=“https://reactjs.org”
+//           target=“_blank”
+//           rel=“noopener noreferrer”
+//         >
+//           Learn React and have fun!
+//         </a>
+//       </header>
+//       <main>
+//         <Title content=“Some simple Title”/>
+//       </main>
+//     </div>
+//   );
+//  }
 
 export default App;
